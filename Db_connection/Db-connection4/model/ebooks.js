0c570152604ec1cf378d518ab3325ec32e321dc9
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-01-14 22:33:53 +0530

    Database connection, Creating structure/model/schema of the database, Retreving the data from the database

---
+++ Db_connection/Db-connection4/model/ebooks.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const BookSchema = mongoose.Schema({
+    id:{
+        type: String
+    },
+    name:{
+        type: String
+    }
+})
+module.exports = mongoose.model("ebooks" , BookSchema)
