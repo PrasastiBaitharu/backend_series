3fc5589109e8e0c3e55a8706596a6f2df9582d7a
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-01-21 20:35:28 +0530

    Getting data from database , searching by _id and id from database

---
+++ Db_connection/Db-connection8/controllers/bookController.js
@@ -0,0 +1,72 @@
+const ebooks = require("../model/ebooks");
+const getAllBooks = async (req, res) => {
+  try {
+    let allBooks = await ebooks.find({});
+    res.json({
+      message: "List of all books",
+      success: "true",
+      data: allBooks,
+    });
+  } catch (error) {
+    res.json({
+      message: "Books not found",
+      success: "false",
+      data: allBooks,
+    });
+  }
+};
+
+const getBooksById = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    console.log(bookId);
+    let allBook = await ebooks.findById(bookId);
+    res.json({
+      message: "Books details",
+      success: "true",
+      data: allBook,
+    });
+  } catch (error) {
+    res.json({
+      message: "Books details",
+      success: "False",
+    });
+  }
+};
+
+const getBooksByChoice = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    console.log(bookId);
+    let allBook = await ebooks.find({ id: bookId });
+    console.log(allBook);
+    res.json({
+      message: "Books details",
+      success: "true",
+      data: allBook,
+    });
+  } catch (error) {
+    res.json({
+      message: "Books details",
+      success: "False",
+    });
+  }
+};
+
+// const addRecord = async (req, res) => {
+//   try {
+//     let newBook = req.body;
+//     const addedBook = await ebooks.insertOne(newBook)
+//     console.log(addedBook);
+//     console.log(newBook);
+//     res.json({
+//       message: "Record added",
+//       success: "true",
+//       data: newBook,
+//     });
+//   } catch (error) {
+
+//   }
+// };
+
+module.exports = { getAllBooks, getBooksById, getBooksByChoice};
