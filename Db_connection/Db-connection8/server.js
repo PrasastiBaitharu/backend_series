0c98fdcc5fa051e2184ccf2634a6ea9f8e333db8
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-01-27 23:04:18 +0530

    Getting data from database , searching by _id and id from database, Creating routes in the different folder/file

---
+++ Db_connection/Db-connection8/server.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+
+const connectDB = require("./db");
+
+connectDB()
+
+const app = express();
+app.use(express.json())
+app.use("/ebooks/api", require("./routes/bookRoutes"))
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server is running in ${port}`);
+})
