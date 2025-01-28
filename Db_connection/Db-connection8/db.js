59472ed06ae23850b894a3e8113c5a18f897c936
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-01-28 22:01:50 +0530

    Getting data from database , searching by _id and id from database, Creating routes in the different folder/file

--- Db_connection/Db-connection8/db.js
+++ Db_connection/Db-connection8/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () =>{
     try {
         await mongoose.connect(process.env.MONGO_URL)
-        console.log("Database connected successfully")
+        console.log("Database connected successfully");
     } catch (error) {
         console.log("Server connection failed");
         process.exit(0);
