7c8693bb1a0cde26628f5c6e5056aadd4cc4e892
PrasastiBaitharu <prasastikumarbaitharu@gmail.com>
2025-02-10 23:35:58 +0530

    Getting all data of users from database

---
+++ Db_connection/Product_user/users/model/users.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    id:{
+        type: String
+    },
+    name:{
+        type:String
+    },
+    username:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    address:{
+        street:{
+            type:String
+        },
+        suite:{
+            type:String
+        },
+        city:{
+            type:String
+        },
+        zipcode:{
+            type:String
+        },
+        geo:{
+            lat:{
+                type:String
+            },
+            lng:{
+                type:String
+            }
+        }
+    },
+    phone:{
+        type:String
+    },
+    website:{
+        type:String
+    },
+    company:{
+        name:{
+            type:String
+        },
+        catchPhrase:{
+            type:String
+        },
+        bs:{
+            type:String
+        }
+    }
+})
+
+module.exports = mongoose.model("users", userSchema);
